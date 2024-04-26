9f7db3d43d48e0336e3e696d6758cf28bce46711
mrBicarato <mr.bicarato@outlook.com>
2024-04-26 10:51:56 -0300

    primeiralinha

--- HealthPoint/src/components/Header.jsx
+++ HealthPoint/src/components/Header.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+import logo from '../assets/Hea.jpg'
+import './Header.css'
+
+
 const Header = () => {
-    return(
-        <h1>saporra</h1>
-    
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <Link to={"/"}><li>Fale Conosco</li></Link>
+                    |
+                    <Link to={"/"}><li>Atendimento por e-mail</li></Link>
+                    |
+                    <Link to={"/"}><li>Nossas Lojas</li></Link>
+                    |
+                    <Link to={"/"}><li>Acompanhe seu pedido</li></Link>
+                    |
+                    <div className="icon-perfil">
+                        <i class="bi bi-person-fill perfil"></i>
+                        <p className="">Olá, Bem vindo! <br /> <Link><span>Entre</span></Link> ou <Link><span>Cadastre-se</span></Link> Aqui</p>
+                    </div>
+                    
+                </ul>
+                <hr />
+            </nav>
+
+        </header>
     )
 }
 
