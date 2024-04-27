5b6877b789732bbad96157c3f0c14ef8693fc53a
GRBicarato <mr.bicarato@outlook.com>
2024-04-27 12:22:27 -0300

    a

--- HealthPoint/src/main.jsx
+++ HealthPoint/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/router/Home.jsx'
 import Error from './components/router/Error-page.jsx'
 
